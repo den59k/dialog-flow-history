c77bbaea6d296f340eeea7607ab6b8007f033bbd
den59k <den59k@gmail.com>
2023-04-10 14:24:51 +0500

    feat: add persons json

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { response } from "../utils/response";
+import fs from 'fs'
 
 export default async (fastify: FastifyInstance) => {
 
@@ -7,11 +8,20 @@ export default async (fastify: FastifyInstance) => {
     return { status: "up" }
   })
 
+  const personsInfo = JSON.parse(fs.readFileSync(process.cwd()+"/persons.json", "utf-8") || "{}")
+  const personsMap = new Map()
+  for (let [ key, value ] of Object.entries<any>(personsInfo)) {
+    personsMap.set(key.toLowerCase(), value)
+    for (let synonim of (value.synonym || [])) {
+      personsMap.set(synonim.toLowerCase(), value)
+    }
+  }
+
   fastify.post("/request", async (req) => {
 
     const body = req.body as any
     const {action, parameters} = body.queryResult
-
+    
     if (action === "electricOnOff") {
       const on = parameters.action === "enable"
       return response([
@@ -22,8 +32,12 @@ export default async (fastify: FastifyInstance) => {
     }
 
     if (action === "whoIs") {
+      const person = personsMap.get(parameters.person.name.toLowerCase())
+      if (person) {
+        return response(person.about)
+      }
       return response([
-        `Я точно не знаю, но думаю, что ${parameters.person} - хороший человек!`
+        `Я точно не знаю, но думаю, что ${parameters.person.name} - хороший человек!`
       ])
     }
 
